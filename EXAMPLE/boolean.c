ad81120e2fbc7f80f23bd2bc125ae5cb4c4baa7a
Natchpai <119587043+Natchpai@users.noreply.github.com>
2024-07-21 14:33:45 +0700

    Create boolean.c

---
+++ EXAMPLE/boolean.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool a = true;
+
+bool funcEven(int x) {
+    return ((x % 2) == 0) ? true : false;
+}
+
+bool funcOdd(int x) {
+    return ((x % 2) == 1) ? true : false;
+}
+
+int main(void) {
+    printf("%d", funcEven(16));
+}
