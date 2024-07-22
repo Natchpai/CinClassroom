3ac146f07af257635a75950255c92eb132966b5a
Natchpai <119587043+Natchpai@users.noreply.github.com>
2024-07-22 09:06:10 +0700

    Update velocaity_cal.c

---
+++ week3/velocaity_cal.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+double velocityCal(double d, double t) {
+    return d / t;
+}
+
+
+void printFormat() {
+    printf("------------------------------\n");
+    printf("     Velocity Calculation     \n");
+    printf("------------------------------\n");
+    printf("     v = distance / time      \n");
+    printf("------------------------------\n");
+
+}
+
+
+int main() {
+
+    double d, t, v;
+
+    printFormat();
+
+    printf("  Distance (m) = ");
+    scanf("%lf", &d);
+
+    printf("  Time (s) = ");
+    scanf("%lf", &t);
+
+    v = velocityCal(d, t);
+
+    printf("  Velocity (m/s) = %.2f", v);
+
+}
